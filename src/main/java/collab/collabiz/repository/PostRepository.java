3ae632c29492052965a9c6dd0a562fe86ac6fd52
SeongcheolAhn <seongcheol.ahn@gmail.com>
2022-04-08 00:21:40 +0900

    Add mvc directory

---
+++ src/main/java/collab/collabiz/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package collab.collabiz.repository;
+
+import collab.collabiz.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
