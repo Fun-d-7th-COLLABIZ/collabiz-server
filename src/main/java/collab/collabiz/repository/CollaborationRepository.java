5d23844e18f7c427ea4e8d9b47f3866711e70cc0
jungdahae1225 <jungdahae1225@gmail.com>
2022-04-27 01:20:32 +0900

    [merge]

--- src/main/java/collab/collabiz/repository/CollaborationRepository.java
+++
@@ -1,7 +0,0 @@
-package collab.collabiz.repository;
-
-import collab.collabiz.entity.Collaboration;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CollaborationRepository extends JpaRepository<Collaboration, Long> {
-}
