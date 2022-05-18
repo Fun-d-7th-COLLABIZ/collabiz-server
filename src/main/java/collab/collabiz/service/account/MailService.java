b4f1bded48405a3fbdd00852cf98889d341113f6
jungdahae1225 <jungdahae1225@gmail.com>
2022-05-18 20:21:35 +0900

    [ADD] mail HTML

--- src/main/java/collab/collabiz/service/account/MailService.java
+++ src/main/java/collab/collabiz/service/account/MailService.java
@@ -6,11 +6,21 @@ import collab.collabiz.entity.account.dtos.AccountResponseDto;
 import collab.collabiz.entity.account.dtos.MailDto;
 import collab.collabiz.repository.MemberRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpSession;
 import java.util.UUID;
 
@@ -20,17 +30,45 @@ import java.util.UUID;
 public class MailService {
 
     private JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
+
     private static final String FROM_ADDRESS = "dahaeSpringstudy@gmail.com";
     private final MemberRepository memberRepository;
 
-    public void mailSend(HttpSession session) {
+    public void mailSend11(HttpSession session) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo((String) session.getAttribute("email"));
         message.setFrom(MailService.FROM_ADDRESS);
 
         message.setSubject("CollaBiz 서비스 사용을 위해 코드를 복사하여 붙여넣어주세요.");
         message.setText("안녕하세요 CollaBiz입니다 다음 문자를 홈페이지에 입력해주세요 => " + (String) session.getAttribute("token"));
+
+
         mailSender.send(message);
+    }
+
+    public void mailSend(HttpSession session) {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8"); // 이미지 넣기 위해 true로
+            mimeMessageHelper.setTo((String) session.getAttribute("email"));
+            mimeMessageHelper.setFrom(MailService.FROM_ADDRESS);
+            mimeMessageHelper.setSubject("CollaBiz 서비스 사용을 위해 코드를 복사하여 붙여넣어주세요.");
+
+            Context context = new Context(); // model에 내용담아주듯이
+            context.setVariable("token",(String) session.getAttribute("token"));
+            context.setVariable("username", (String) session.getAttribute("email"));
+            context.setVariable("message","CollaBiz 서비스 사용을 위해 코드를 복사하여 붙여넣어주세요.");
+
+            String message = templateEngine.process("mail/emailAuth_Template", context);
+
+            mimeMessageHelper.setText(message,true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+        mailSender.send(mimeMessage);
     }
 
     public String generateEmailCheckToken() {
